e0c81298ade4b8792965247a4759eb553889d04d
Nnamdi Michael Okpala <obinexus@tuta.com>
2024-12-12 00:11:05 +0000

    init

--- gosilang/src/core/tokenizer/symbols/symbol_type.h
+++ gosilang/src/core/tokenizer/symbols/symbol_type.h
@@ -1,6 +1,14 @@
-#ifndef SYMBOL_TYPE_H
-#define SYMBOL_TYPE_H
+#ifndef SYM_TYPE_H
+#define SYM_TYPE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// For Compile-Type safety,I have seperated the
+// the token type and token value in two modules
+// sym_type and sym_value.
 
 
 
-#endif // SYMBOL_TYPE_H
+#endif // SYM_TYPE_H
